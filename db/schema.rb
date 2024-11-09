7b41b34887e9c82c9cabc29de01c77c707f47d2e
ramesh-exalogic <ramesh.kumar@exalogic.co>
2024-11-09 13:33:38 +0530

    Added user-id column in article table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_10_31_110503) do
+ActiveRecord::Schema[7.0].define(version: 2024_11_09_080026) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -47,6 +47,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_10_31_110503) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "audits", force: :cascade do |t|
